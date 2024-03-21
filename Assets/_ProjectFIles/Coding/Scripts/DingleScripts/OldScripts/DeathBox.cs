643923c4dec40186eb5c8421093d726b5ddaa1f6
Taad874 <Donnelly-A73@ulster.ac.uk>
2024-03-21 09:55:27 +0000

    Merged Branches, added scripts
    Man this was hell, but Levels 1 and 3 should now be using the checkpoint scripts I have.

--- Assets/_ProjectFIles/Coding/Scripts/DingleScripts/OldScripts/DeathBox.cs
+++ Assets/_ProjectFIles/Coding/Scripts/DingleScripts/OldScripts/DeathBox.cs
@@ -1,21 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
 
-public class DeathBox : MonoBehaviour
-{
-    [SerializeField] Collider2D collision;
-    private void Awake()
-    {
-        collision = GetComponent<Collider2D>();
-    }
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            SceneManager.LoadScene(0);
+//public class DeathBox : MonoBehaviour
+//{
 
-        }
-    }
-}
+//    void OnTriggerEnter2D(Collider2D other)
+//    {
+//        if (other.CompareTag("Player"))
+//        {
+//            SceneManager.LoadScene(0);
+
+//        }
+//    }
+//}
